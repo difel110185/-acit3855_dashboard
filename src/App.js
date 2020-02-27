bf8ebea906653c2f7b39795df1d7b79f58279985
Diego Felix <dfelix@wkm.com.br>
2020-02-27 09:42:21 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import ball from './ball.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [firstGoalScored, setFirstGoalScored] = useState(undefined);
+    const [stats, setStats] = useState(undefined);
+
+    useEffect(() => {
+        axios.get('http://localhost:8110/events/nth_goal_reported?position=0').then(response => {
+            setFirstGoalScored(response.data);
+        })
+    }, []);
+
+    const getStats = () => {
+        axios.get('http://0.0.0.0:8100/events/stats').then(response => {
+            setStats(response.data);
+        })
+    };
+
+    useEffect(() => {
+        setTimeout(getStats, 2000)
+    }, [stats]);
+
+    return (
+        <div className="App">
+            <header className="App-header"><img src={ball} className="App-logo" alt="logo" /></header>
+
+            <h3>Number of goals scored</h3>
+            {stats && stats.num_goals_scored}
+
+            <h3>Number of cards received</h3>
+            {stats && stats.num_cards_received}
+
+            <h3>First goal scorer</h3>
+            {firstGoalScored && firstGoalScored.player}
+
+            <h3>Last updated</h3>
+            {stats && stats.updated_timestamp}
+        </div>
+    );
 }
 
 export default App;
